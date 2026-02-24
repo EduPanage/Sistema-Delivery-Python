65c3918a5d45addfc4981aae0cb9324fff3ee386
Eduardo <panagee3@gmail.com>
2026-02-24 16:33:31 -0400

    Modularizacao do Projeto

--- app.py
+++ app.py
@@ -1,34 +1,8 @@
-import os
-from function import end_app
-from function import add_restaurant
+#Funcao para Execucao da Funcao os.system('cls')
+import os     
 
-print("""
-      
-██████╗░███████╗██╗░░░░░██╗██╗░░░██╗███████╗██████╗░███╗░░██╗░█████╗░░██╗░░░░░░░██╗
-██╔══██╗██╔════╝██║░░░░░██║██║░░░██║██╔════╝██╔══██╗████╗░██║██╔══██╗░██║░░██╗░░██║
-██║░░██║█████╗░░██║░░░░░██║╚██╗░██╔╝█████╗░░██████╔╝██╔██╗██║██║░░██║░╚██╗████╗██╔╝
-██║░░██║██╔══╝░░██║░░░░░██║░╚████╔╝░██╔══╝░░██╔══██╗██║╚████║██║░░██║░░████╔═████║░
-██████╔╝███████╗███████╗██║░░╚██╔╝░░███████╗██║░░██║██║░╚███║╚█████╔╝░░╚██╔╝░╚██╔╝░
-╚═════╝░╚══════╝╚══════╝╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░░
-      
-      """)
+#Import main do function.py
+from function import main            
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-option = int(input('Escolha uma das opções: '))
-
-print(f'\nOpcao Escolhida: {option}\n')
-
-if option == 1:
-    add_restaurant()
-elif option == 2:
-    os.system('cls')
-    print('Operacao Escolhida: Listagem')
-elif option == 3:
-    os.system('cls')
-else:
-    end_app()
-    
+if __name__ == '__main__':   #Inicializando o sistema
+    main()
--- function.py
+++ function.py
@@ -1,5 +1,47 @@
-import os
-import time
+import os       #Funcao para Execucao da Funcao os.system('cls')
+import time     #Funcao para Execucao da Funcao time.sleep
+
+
+def main():
+    show_app()
+    show_options()
+    operation()
+
+
+def show_app():
+        print("""
+        
+    ██████╗░███████╗██╗░░░░░██╗██╗░░░██╗███████╗██████╗░███╗░░██╗░█████╗░░██╗░░░░░░░██╗
+    ██╔══██╗██╔════╝██║░░░░░██║██║░░░██║██╔════╝██╔══██╗████╗░██║██╔══██╗░██║░░██╗░░██║
+    ██║░░██║█████╗░░██║░░░░░██║╚██╗░██╔╝█████╗░░██████╔╝██╔██╗██║██║░░██║░╚██╗████╗██╔╝
+    ██║░░██║██╔══╝░░██║░░░░░██║░╚████╔╝░██╔══╝░░██╔══██╗██║╚████║██║░░██║░░████╔═████║░
+    ██████╔╝███████╗███████╗██║░░╚██╔╝░░███████╗██║░░██║██║░╚███║╚█████╔╝░░╚██╔╝░╚██╔╝░
+    ╚═════╝░╚══════╝╚══════╝╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░░
+        
+        """)
+    
+def show_options():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+
+def operation():
+    option = int(input('Escolha uma das opções: '))
+
+    print(f'\nOpcao Escolhida: {option}\n')
+    
+    if option == 1:         #Funcoes do Function.py
+        add_restaurant()
+    elif option == 2:
+        os.system('cls')
+        print('Operacao Escolhida: Listagem')
+    elif option == 3:
+        os.system('cls')
+    else:
+        end_app()
+
 
 def add_restaurant():
     os.system('cls')
